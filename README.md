4564bb58dfb797f87873776cfdc6573f995e57ba
Rafael Soto <rafael.soto@gmail.com>
2016-09-23 20:08:30 -0300

    Apontando para os serviços

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PNDF-APP
+
+##Comandos Ionic
+* $ionic start myApp tabs -> Cria uma aplicação baseado no template tabs
+* $ionic upload -> Publica o projeto no Ionic
+* $ionic serve -> Sobe uma versão local para teste
+
+## Tutorial Ionic
+* http://appcamp.io/courses/user-interface/list-refresh
--- www/js/app.js
+++ www/js/app.js
@@ -1,56 +1,22 @@
 
 angular.module('app', ['ionic'])
 
-.controller('ListagemCtrl', function($scope, ChatService) {
-  $scope.chats = ChatService.getChats();
+.controller('ListagemCtrl', function($scope, BackendService) {
+    BackendService.getPercursos()
+   .then(function(response) {
+     $scope.chats = response;
+   });
+
 })
 
-.controller('AvisoCtrl', function($scope, $stateParams, ChatService) {
+.controller('AvisoCtrl', function($scope, $stateParams, BackendService) {
    $scope.chatId = $stateParams.chatId;
-   $scope.chat = ChatService.getChat($scope.chatId);
+   $scope.chat = BackendService.getPercurso($scope.chatId).then(function(response) {
+     $scope.chat = response;
+   });
 })
 
-.service('ChatService', function() {
- return {
-   chats: [
-     {
-       id: "1",
-       title: "Pedal 21/09/2016",
-       message: "Pedal Gastronômico - 22Km HOJE (Quarta 21.09) Pizza na pedra"
-     },
-     {
-      id: "2",
-      title: "Pedal 20/09/2016",
-      message: "Pedal Gastronômico - 22Km HOJE (Quarta 21.09) Pizza na pedra"
-     },
-     {
-      id: "3",
-       title: "Pedal Cancelado 19/09/2016",
-       message: "Pedal Gastronômico - 22Km HOJE (Quarta 21.09) Pizza na pedra"
-     },
-     {
-       id: "4",
-       title: "Pedal 16/09/2016",
-       message: "Pedal Gastronômico - 22Km HOJE (Quarta 21.09) Pizza na pedra"
-     },
-     {
-       id: "5",
-       title: "Pedal 15/09/2016",
-       message: "Pedal Gastronômico - 22Km HOJE (Quarta 21.09) Pizza na pedra"
-     }
-   ],
-   getChats: function() {
-     return this.chats;
-   },
-   getChat: function(chatId) {
-     for(i=0;i<this.chats.length;i++){
-       if(this.chats[i].id == chatId){
-         return this.chats[i];
-       }
-     }
-   }
- }
-})
+
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -67,4 +33,27 @@ angular.module('app', ['ionic'])
    });
 
  $urlRouterProvider.otherwise("/listagem");
+})
+
+.service('BackendService', function($http) {
+ return {
+   getPercursos: function() {
+     // $http returns a promise, which has a then function, which also returns a promise.
+     return $http.get('https://glacial-thicket-18823.herokuapp.com/percursos')
+       .then(function (response) {
+         // In the response, resp.data contains the result. Check the console to see all of the data returned.
+         console.log('Get Post', response);
+         return response.data;
+       });
+   },
+   getPercurso: function(percursoID) {
+     // $http returns a promise, which has a then function, which also returns a promise.
+     return $http.get('https://glacial-thicket-18823.herokuapp.com/percurso/'+percursoID)
+       .then(function (response) {
+         // In the response, resp.data contains the result. Check the console to see all of the data returned.
+         console.log('Get Post', response);
+         return response.data;
+       });
+   } 
+ };
 })
